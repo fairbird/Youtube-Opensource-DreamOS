8beeddac446e4ebd60b9b01e9f1104a28f583203
RAED <rrrr53@hotmail.com>
2023-02-21 22:16:02 +0300

    Add Update Online Feature

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 -------
 [![](https://user-images.githubusercontent.com/1623947/79113396-8004a100-7d89-11ea-9ff5-17ea7ce607a6.jpg)](https://www.youtube.com/)
 =========
+To install plugin directly online from telnet be this command ... لتثبيت البلجن مباشرة من خلال الإنترنيت بواسطة التلنت بهذا الأمر
+```
+wget https://raw.githubusercontent.com/fairbird/Youtube-Opensource-DreamOS/master/installer.sh -qO - | /bin/sh
+```
+=========
 Enigma2 plugin to manage your youtube account and watch videos
-
-Depends
--------
-python-codecs, python-json, python-netclient
 
 Privacy policy
 -------
@@ -15,13 +16,3 @@ Privacy policy
 Screenshots
 -------
 ![](https://user-images.githubusercontent.com/1623947/79112992-8f371f00-7d88-11ea-8858-480c5fe94faa.jpg)
-
-![](https://user-images.githubusercontent.com/1623947/79112997-91997900-7d88-11ea-903e-1502e1dbbd9c.jpg)
-
-![](https://user-images.githubusercontent.com/1623947/79113003-978f5a00-7d88-11ea-8b18-60b04ec0e3c7.jpg)
-
-![](https://user-images.githubusercontent.com/1623947/79113010-99f1b400-7d88-11ea-985f-c9a45628b4b6.jpg)
-
-![](https://user-images.githubusercontent.com/1623947/79113014-9c540e00-7d88-11ea-9281-e5788dac44ba.jpg)
-
-
---
+++ installer.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+# ###########################################
+# SCRIPT : DOWNLOAD AND INSTALL YouTube
+# ###########################################
+#
+# Command: wget https://raw.githubusercontent.com/fairbird/Youtube-Opensource-DreamOS/master/installer.sh -qO - | /bin/sh
+#
+# ###########################################
+
+###########################################
+# Configure where we can find things here #
+TMPDIR='/tmp'
+PLUGINDIR='/usr/lib/enigma2/python/Plugins/Extensions'
+
+#######################
+# Remove Old Version #
+rm -rf $PLUGINDIR/YouTube
+rm -rf $TMPDIR/*master*
+
+#########################
+if [ -f /etc/opkg/opkg.conf ]; then
+    STATUS='/var/lib/opkg/status'
+    OSTYPE='Opensource'
+    OPKG='opkg update'
+    OPKGINSTAL='opkg install'
+elif [ -f /etc/apt/apt.conf ]; then
+    STATUS='/var/lib/dpkg/status'
+    OSTYPE='DreamOS'
+    OPKG='apt-get update'
+    OPKGINSTAL='apt-get install'
+fi
+
+#########################
+install() {
+    if grep -qs "Package: $1" $STATUS; then
+        echo
+    else
+        $OPKG >/dev/null 2>&1
+        echo "   >>>>   Need to install $1   <<<<"
+        echo
+        if [ $OSTYPE = "Opensource" ]; then
+            $OPKGINSTAL "$1"
+            sleep 1
+            clear
+        elif [ $OSTYPE = "DreamOS" ]; then
+            $OPKGINSTAL "$1" -y
+            sleep 1
+            clear
+        fi
+    fi
+}
+
+#########################
+if python --version 2>&1 | grep -q '^Python 3\.'; then
+    echo ":You have Python3 image ..."
+    sleep 1
+    Packagescodecs=python3-codecs
+    Packagecore=python3-core
+    Packagejson=python3-json
+    Packagenetclient=python3-netclient
+    Packagepyopenssl=python3-pyopenssl
+    Packagetwistedweb=python3-twisted-web
+else
+    echo ":You have Python2 image ..."
+    sleep 1
+    Packagescodecs=python-codecs
+    Packagecore=python-core
+    Packagejson=python-json
+    Packagenetclient=python-netclient
+    Packagepyopenssl=python-pyopenssl
+    Packagetwistedweb=python-twisted-web
+fi
+
+# check depends packges if installed
+install $Packagescodecs
+install $Packagecore
+install $Packagejson
+install $Packagenetclient
+install $Packagepyopenssl
+install $Packagetwistedweb
+
+#########################
+cd $TMPDIR
+set -e
+echo "Downloading And Insallling YouTube plugin Please Wait ......"
+echo
+wget https://github.com/fairbird/Youtube-Opensource-DreamOS/archive/refs/heads/master.tar.gz -qP $TMPDIR
+tar -xzf master.tar.gz
+mkdir -p $PLUGINDIR/YouTube
+cp -rf Youtube-Opensource-DreamOS-master/src/* $PLUGINDIR/YouTube
+rm -rf *master*
+set +e
+cd ..
+#########################
+# Add latest hash commit
+repo_url="https://github.com/fairbird/Youtube-Opensource-DreamOS/commits/master/src"
+hashfile="/usr/lib/enigma2/python/Plugins/Extensions/YouTube/.hashfile"
+[ ! $hashfile ] && touch $hashfile
+wget -q -O- $repo_url | sed -ne 's#.*data-url="/fairbird/Youtube-Opensource-DreamOS/commits/\([^$<]*\)/commits_list_item".*#\1#p' | cut -d "=" -f2 | awk '{$1=$1};1' | head -1 > $hashfile
+#########################
+
+sleep 1
+clear
+echo "#########################################################"
+echo "#           YouTube INSTALLED SUCCESSFULLY              #"
+echo "#                 Taapat  &  RAED                       #"
+echo "#########################################################"
+echo "#           your Device will RESTART Now                #"
+echo "#########################################################"
+
+if [ $OSTYPE = "Opensource" ]; then
+    killall -9 enigma2
+else
+    systemctl restart enigma2
+fi
+
+exit 0
